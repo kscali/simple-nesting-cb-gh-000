a7b60417b7753b8123b2393b0cd2177030c035ec
Koy Saeteurn <kosatun@aol.com>
2018-07-28 05:39:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,11 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  programmer_hash[0] = {
+ :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
